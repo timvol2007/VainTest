61df577cdbedfbedb45a9620112df35976cee79b
Timo <timo.vollhardt@gmail.com>
2026-01-29 11:06:02 +0100

    Update json.lua

--- src/json/json.lua
+++ src/json/json.lua
@@ -1,1 +1,23 @@
-
+{
+  "uiSettings": {
+    "scale": 1.0,
+    "animationSpeed": 0.3,
+    "theme": "dark"
+  },
+  "modules": {
+    "combat": {
+      "killAura": {
+        "enabled": false,
+        "keybind": "Q",
+        "range": 50,
+        "speed": 100
+      }
+    },
+    "visuals": {
+      "esp": {
+        "enabled": true,
+        "keybind": "E"
+      }
+    }
+  }
+}
